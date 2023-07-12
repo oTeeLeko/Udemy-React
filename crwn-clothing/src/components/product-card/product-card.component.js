fcdc570a0b80ffdcf92cdfc0ec04a7fb0d6cdb70
Supakan Sriwichai <leksupakan@gmail.com>
2023-07-12 11:01:08 +0700

    added in our addToCart feature

--- crwn-clothing/src/components/product-card/product-card.component.js
+++ crwn-clothing/src/components/product-card/product-card.component.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
+import { CartContext } from "../../contexts/card.context";
 
 const ProductCard = function ({ product }) {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} />
@@ -10,7 +15,9 @@ const ProductCard = function ({ product }) {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
